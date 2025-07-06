e9fb9d5ef8f962f237b5a05a2a6dc6ec8f241204
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-05 21:49:12 -0700

    updated src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import DigitalClock from './components/DigitalClock';
+import ThemeToggle from './components/ThemeToggle';
+import FormatToggle from './components/FormatToggle';
+import './styles/Glass.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="glass-card">
+        <ThemeToggle />
+        <DigitalClock />
+        <FormatToggle />
+      </div>
     </div>
   );
 }
